0d24817cdb872c70acc322ca97605d04d0f8705c
GopherGoldIT <info@gophergoldit.com>
2020-02-09 12:43:19 +0200

    Basic Setup

---
+++ DVTWeather/Resources/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  DVTWeather
+//
+//  Created by Shaun Churchyard on 2020/02/09.
+//  Copyright © 2020 Shaun Churchyard. All rights reserved.
+//
+
+import UIKit
+
+struct K{
+
+//MARK: - openweathermap constants
+    //base should be eg //"https://api.openweathermap.org/data/2.5/weather?appid=key&units=metric"
+    static let openweathermap_app_key = "appid=key"
+    static let openweathermap_base = "https://api.openweathermap.org/data/2.5/weather"
+    static let openweathermap_units = "&units=metric"
+
+   
+}
---
+++ DVTWeather/Services/OpenWeather/WeatherModel.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherModel.swift
+//  DVTWeather
+//
+//  Created by Shaun Churchyard on 2020/02/09.
+//  Copyright © 2020 Shaun Churchyard. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+    
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+    }
+    
+}
